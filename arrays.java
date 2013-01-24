719b5ca56d39145d20f39a07b8e86d26f232ab58
Matthew Price <doggy1199@yahoo.com>
2013-01-24 08:35:14 -0700

    Initial Commit

---
+++ arrays.java
@@ -0,0 +1,18 @@
+
+class arrays {
+
+	public static void main(String[] args) {
+		int[] nums = {3, 6, 4, 3, 2, 1, 5, 6};
+
+		System.out.println(avg(nums));
+	}
+
+	public static double avg(int[] nums) {
+		double temp = 0;
+		for(int i = 0; i < nums.length; i++) {
+			temp += nums[i];
+		}
+		return temp/nums.length;
+	}
+
+}
