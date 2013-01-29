13df2c93f08bfd32f16f40bf7af5f32c5101f1f9
Matthew Price <doggy1199@yahoo.com>
2013-01-29 07:59:16 -0700

    Java doc

--- arrays.java
+++ arrays.java
@@ -1,21 +1,46 @@
 
 class arrays {
 
+	/**
+	 * 		arrays
+	 *
+	 * 			Methods for dealing with arrays
+	 *
+	 * 		Matthew Price
+	 */
+
 	public static void main(String[] args) {
 		int[] nums = {3, 6, 4, 3, 2, 1, 5, 6};
 
 		System.out.println(avg(nums));
 	}
 
+
+	/**
+	 *avg
+	 *		Finds the mean of an array
+	 *
+	 * @param nums - the array
+	 * @return the mean
+	 */
 	public static double avg(int[] nums) {
 		return sum(nums)/nums.length;
 	}
 
-	public static int dum(int[] nums) {
-		double temp = 0;
+	/**
+	 * sum
+	 * 		Finds the sum of an array
+	 *
+	 * @param nums - the array
+	 * @return the sum
+	 */
+
+	public static int sum(int[] nums) {
+		int temp = 0;
 		for(int i = 0; i < nums.length; i++) {
 			temp += nums[i];
 		}
 		return temp;
 	}
+	
 }
