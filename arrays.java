9e0e325c06a0e6d10db579264df05661f585825a
Matthew Price <doggy1199@yahoo.com>
2013-01-29 08:24:25 -0700

    moved files

--- arrays.java
+++ arrays.java
@@ -12,7 +12,7 @@ class arrays {
 	public static void main(String[] args) {
 		int[] nums = {3, 6, 4, 3, 2, 1, 5, 6};
 
-		System.out.println(avg(nums));
+		System.out.println(threes(nums));
 	}
 
 
@@ -41,6 +41,18 @@ class arrays {
 			temp += nums[i];
 		}
 		return temp;
+	}
+
+
+
+	public static int threes(int[] nums) {
+		int numThrees = 0;
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] == 3) {
+				numThrees++;
+			}
+		}
+		return numThrees;
 	}
 	
 }
